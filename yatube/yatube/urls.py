2dfdecc59a2c90e73eaa6276c223d52a2c36a47e
Надежда Белова <belovanadezhda1983@gmail.com>
2023-03-02 13:53:15 +0300

    Add static MEDIA dir if debug

--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("auth/", include("users.urls")),
@@ -25,3 +27,9 @@ urlpatterns = [
     # path('__debug__/', include('debug_toolbar.urls')),  # delete this.RGenius
 ]
 handler404 = 'core.views.page_not_found'
+
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+    )
