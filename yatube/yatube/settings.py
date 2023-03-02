fc8f52638ff294f20f194d67caf6d13e29d9634d
Надежда Белова <belovanadezhda1983@gmail.com>
2023-03-02 13:53:42 +0300

    Add media dir

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -157,3 +157,9 @@ ALLOWED_HOSTS = [
 ]
 
 CSRF_FAILURE_VIEW = 'core.views.csrf_failure'
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/2.2/howto/static-files/
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
